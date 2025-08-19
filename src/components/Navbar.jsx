9f81175b1ac5d79e867df065b2a8ac49e9532ff9
Edi Aquino de Souza <edsouzza@hotmail.com>
2025-08-19 20:37:52 -0300

    finalizada navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <nav id="navbar">
+            <h2>
+                <Link to="/">
+                    <BiCameraMovie /> MoviesLib
+                </Link>
+            </h2>
+           <form>
+             <input type="text" placeholder="Busque um filme" />
+             <button type="submit"><BiSearchAlt2 /></button>
+           </form>
+        </nav>
+    )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+
+import Home from   "./pages/Home"
+import Movie from  "./pages/Movie"
+import Search from "./pages/Search"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+         <Route element={<App />}>
+            <Route path='/' element={<Home />} />
+            <Route path='movie/:id' element={<Movie />} />
+            <Route path='search' element={<Search />} />
+         </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
